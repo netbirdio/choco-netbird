6554a3768743313a0142c2eb8a498b85b4a50ae6
Daniils Kostornijs <cosmaori@gmail.com>
2024-01-08 18:59:15 +0100

    Rolled back version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName = 'netbird client *'
 
-  checksum64 = '69ec7a4c73c8ecb6d0a800a24c339bdde9e6600d71547bf47974ac6b495fa250'
+  checksum64 = '3b102b2f82320dadca2424086d9d5548596b7b476246370af638707ff01446e7'
   checksumType64 = 'sha256'
 
   silentArgs = "/qn"
