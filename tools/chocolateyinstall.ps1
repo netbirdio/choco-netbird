92a38e776f726394e546e76fe01f41398154bf0b
mlsmaycon <mlsmaycon@users.noreply.github.com>
2025-10-01 23:29:36 +0000

    Bumping NetBird version to 0.59.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version = '0.58.2'
+$version = '0.59.0'
 $url64 = "https://github.com/netbirdio/netbird/releases/download/v${version}/netbird_installer_${version}_windows_amd64.msi"
 
 $packageArgs = @{
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName = 'NetBird*'
 
-  checksum64 = 'a7bd111804ce68467a0315dd9f5e50532a3ab41637a8bc39ea184091e8b03f0b'
+  checksum64 = '75b058e5f8587626517a60049f33fa5297f6490d2928a94866999292b567c097'
   checksumType64 = 'sha256'
 
   silentArgs = "/qn"
